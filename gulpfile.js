02a9a54fa6ece70d6360ce0bb7ef1ef1c46b9856
Stephane <stephane.mullings@gmail.com>
2016-10-22 03:57:38 +0200

    mise en place du stack front

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 const elixir = require('laravel-elixir');
 
+var fs = require('fs');
+var path = require('path');
+
+var gulp = require('gulp');
+
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+var plugins = require('gulp-load-plugins')();
+
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+var dirs = pkg['h5bp-configs'].directories;
+var cleanCSS = require('gulp-clean-css');
 require('laravel-elixir-vue');
 
 /*
@@ -13,7 +29,17 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+
+
+gulp.task('sass', function(){
+    gulp.src(dirs.src + '/sass/*.scss')
+        .pipe(plugins.sass())
+        .pipe(plugins.csscomb())
+        .pipe(plugins.cssbeautify({indent: '  '}))
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 8', '> 1%'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(dirs.dist + '/css'));
 });
