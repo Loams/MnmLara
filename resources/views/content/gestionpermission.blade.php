60d283c56571059490f6294984a2b473501032f4
Stephane <stephane.mullings@gmail.com>
2016-10-30 13:30:16 +0100

    mise en place de l'affichage de la gestion des permission 1 vue

--- resources/views/content/gestionpermission.blade.php
+++ resources/views/content/gestionpermission.blade.php
@@ -6,57 +6,77 @@
 @endsection
 
 @section('content')
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-            <div class="x_title">
-                <h2>Responsive example
-                    <small>Users</small>
-                </h2>
-                <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-                <p class="text-muted font-13 m-b-30">
-                    Responsive is an extension for DataTables that resolves that problem by optimising the table's
-                    layout for different screen sizes through the dynamic insertion and removal of columns from the
-                    table.
-                </p>
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        {!! Form::model($permissions, ['route'=>['gestionpermission.update'], 'method'=>'put', 'class'=>'form-horizontal panel']) !!}
-                        @foreach($roles as $role)
 
-                            <div class="form-group">
-                                <label class="col-md-3 col-sm-3 col-xs-12 control-label">{{ $role->display_name }}
-                                    <br>
-                                    <small class="text-navy">{{ $role->description }}</small>
-                                </label>
-                                <div class="col-md-9 col-sm-9 col-xs-12">
-                                    @foreach($permissions as $permission)
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox" class="flat"
-                                                       @if($permission->hasRole($role->id) != null && !$permission->hasRole($role->id)->isEmpty()) checked="checked"
-                                                       @endif name="{{ $role->name }}_{{ $permission->name }}"> {{ $permission->display_name }}
-                                            </label>
-                                        </div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        @endforeach
+        <div class="col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2><i class="fa fa-bars"></i> Vertical Tabs <small>Float left</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Settings 1</a>
+                                </li>
+                                <li><a href="#">Settings 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a class="close-link"><i class="fa fa-close"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                    <div class="col-xs-3">
+                        <!-- required for floating -->
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs tabs-left">
+                            @foreach($roles as $role)
+                            <li @if($loop->first) class="active" @endif><a href="#{{ $role->id }}" data-toggle="tab">{{ $role->display_name }}</a>
+                            </li>
+                           @endforeach
+                        </ul>
                     </div>
+
+                    <div class="col-xs-9">
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            @foreach($roles as $role)
+                            <div class="tab-pane @if($loop->first)active @endif" id="{{ $role->id }}">
+                                <p class="lead">{{ $role->display_name }}</p>
+                                {!! Form::model($role, ['route'=>['role.updatepermission', $role->id], 'method'=>'put', 'class'=>'form-horizontal panel']) !!}
+                                @foreach($permissions as $permission)
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" class="flat"
+                                                   @if($permission->hasRole($role->id) != null && !$permission->hasRole($role->id)->isEmpty()) checked="checked"
+                                                   @endif name="{{ $permission->id }}"> {{ $permission->display_name }}
+                                        </label>
+                                    </div>
+                                @endforeach
+                                {!! Form::submit('Envoyer', ['class'=>'btn btn-primary pull-right']) !!}
+                                {!! Form::close() !!}
+                            </div>
+
+                            @endforeach
+
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
                 </div>
             </div>
         </div>
 
+        @endsection
 
-        {!! Form::submit('Envoyer', ['class'=>'btn btn-primary pull-right']) !!}
-        {!! Form::close() !!}
-    </div>
-    @endsection
+        @section('script')
 
-    @section('script')
-
-            <!-- iCheck -->
+                <!-- iCheck -->
     <script src="{{ url('/') }}/vendors/iCheck/icheck.min.js"></script>
     <script>
         $('input').iCheck({
